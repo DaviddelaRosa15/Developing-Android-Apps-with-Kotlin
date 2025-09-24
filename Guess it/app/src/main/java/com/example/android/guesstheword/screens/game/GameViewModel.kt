897593ef2f055353c70473212e7131aa30223d9c
David N. de la Rosa L. <89267615+DaviddelaRosa15@users.noreply.github.com>
2025-09-24 12:03:48 -0400

    Exercise: Add LiveData Encapsulation to GameViewModel
    Changed word and score properties to private MutableLiveData and exposed them as public LiveData. This improves encapsulation and ensures that only the ViewModel can modify these values.

--- Guess it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Guess it/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,6 +1,7 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import kotlin.dec
@@ -9,10 +10,14 @@ import kotlin.inc
 class GameViewModel : ViewModel() {
 
     // The current word
-    var word = MutableLiveData<String>()
+    private var _word = MutableLiveData<String>()
+    val word : LiveData<String>
+        get() = _word
 
     // The current score
-    var score = MutableLiveData<Int>()
+    private var _score = MutableLiveData<Int>()
+    val score : LiveData<Int>
+        get() = _score
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -21,7 +26,7 @@ class GameViewModel : ViewModel() {
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
-        score.value = 0
+        _score.value = 0
     }
 
     override fun onCleared() {
@@ -67,19 +72,19 @@ class GameViewModel : ViewModel() {
         if (wordList.isEmpty()) {
             //gameFinished()
         } else {
-            word.value = wordList.removeAt(0)
+            _word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score.value = (score.value)?.dec()
+        _score.value = (score.value)?.dec()
         nextWord()
     }
 
     fun onCorrect() {
-        score.value = (score.value)?.inc()
+        _score.value = (score.value)?.inc()
         nextWord()
     }
 }
